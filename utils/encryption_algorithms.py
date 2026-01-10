d6ad5aac53d83a7e86b66b0e088b723afc6fa6d2
adasrakieta <szymon.przybysz2003@gmail.com>
2026-01-10 23:04:58 +0100

    feat: Remove encryption algorithms and routes; update project structure

---
+++ utils/encryption_algorithms.py
@@ -0,0 +1,30 @@
+"""
+Encryption and Cryptography Algorithms Module
+==============================================
+
+"""Encryption algorithms have been removed from the project."""
+
+# Placeholder symbols to avoid import errors if referenced inadvertently
+class ClassicalCiphers:
+    pass
+
+
+class ModernEncryption:
+    pass
+
+
+class HashingFunctions:
+    pass
+
+
+class EncodingFunctions:
+    pass
+
+
+def generate_substitution_map():
+    return {}
+
+
+def validate_substitution_map(substitution_map):
+    return False
+    def caesar_encrypt(text, shift):
